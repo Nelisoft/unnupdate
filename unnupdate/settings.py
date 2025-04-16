9f0614e37baade995f83283a7c4a72ec64675af7
Nelisoft <57540765+Nelisoft@users.noreply.github.com>
2025-04-16 10:32:27 -0700

    Update settings.py

--- unnupdate/settings.py
+++ unnupdate/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$zs=(p4s%9n#d-@b=sqz)00@s6lq3s11(5nmm5*&2&!j7%$ng9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost']
 
 
 # Application definition
